dab66d8b8fad657a2c9fd05e4fb765d23617f9e3
Ravin Sardal <ravin.sardal@gmail.com>
2016-03-07 09:38:13 -0800

    Added basic http server

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
+
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub=
 		System.out.println("Hello World!!");
+		HTTPServer.startServer();
 	}
 
 }
