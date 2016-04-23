a7543931d5fe3bd57899cacbad08085a4860fd50
Ravin Sardal <ravin.sardal@gmail.com>
2016-04-23 15:26:09 -0700

    Add skeleton controllers for Profile, Search, Sell

---
+++ mobile/SjsuBookFinder/SjsuBookFinder/SellViewController.m
@@ -0,0 +1,37 @@
+//
+//  SellViewController.m
+//  SjsuBookFinder
+//
+//  Created by Ravin Sardal on 4/23/16.
+//  Copyright © 2016 Ravin Sardal. All rights reserved.
+//
+
+#import "SellViewController.h"
+
+@interface SellViewController ()
+
+@end
+
+@implementation SellViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
