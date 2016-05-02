84b99d20881dfc9b953a40e1646a81866946b528
Ravin Sardal <ravin.sardal@gmail.com>
2016-05-02 08:40:38 -0700

    Create findBookParser class

---
+++ mobile/SjsuBookFinder/SjsuBookFinder/FindBookParser.h
@@ -0,0 +1,13 @@
+//
+//  FindBookParser.h
+//  SjsuBookFinder
+//
+//  Created by Ravin Sardal on 5/2/16.
+//  Copyright © 2016 Ravin Sardal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FindBookParser : NSObject
+
+@end
---
+++ mobile/SjsuBookFinder/SjsuBookFinder/FindBookParser.m
@@ -0,0 +1,13 @@
+//
+//  FindBookParser.m
+//  SjsuBookFinder
+//
+//  Created by Ravin Sardal on 5/2/16.
+//  Copyright © 2016 Ravin Sardal. All rights reserved.
+//
+
+#import "FindBookParser.h"
+
+@implementation FindBookParser
+
+@end
